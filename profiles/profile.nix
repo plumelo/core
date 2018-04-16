20653069bd3556a8a53a5dd19470af5df0b1c507
vasile <elsile69@yahoo.com>
2018-04-16 13:32:20 +0300

    Use kernel 4.16
    Build cores 12
    
    
    Kernel
    
    
    kernel 4.16

--- profiles/profile.nix
+++ profiles/profile.nix
@@ -3,6 +3,14 @@
   imports = [
     ./local.nix
     ./plumelo.nix
-    ../modules/services/X11/gnome3.nix 
+    ../modules/services/X11/gnome3.nix
   ];
+
+  nix.buildCores = 10;
+  boot = {
+    kernelModules  = [
+      "nct6775"
+    ];
+    kernelPackages = pkgs.linux_4_16;
+  };
 }
