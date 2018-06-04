a1f3eb1f071e95daf73971ee70ab1db4855ed350
Iulian Meghea <iulian.meghea@gmail.com>
2018-06-04 10:44:53 +0300

    Update it87

--- pkgs/linux/it87.nix
+++ pkgs/linux/it87.nix
@@ -10,8 +10,8 @@ in {
       src = fetchFromGitHub { 
         owner = "groeck"; 
         repo = "it87"; 
-        rev = "3436b280785dbb05a73c5468d09d1e49e8ce09c9"; 
-        sha256 = "1abp7jpacklhhmn1c265yyzgp8dph6cdjpgbwhvx8lf1xh6vzqdz"; 
+        rev = "71826085db0fc343eac84d153018c6ee2b96629e"; 
+        sha256 = "1cf6765q8na4fr7lxqcsi5q782nc8an98hipkbpgwial5hdl1lhc"; 
       }; 
       hardeningDisable = ["pic"]; 
      nativeBuildInputs = kernel.moduleBuildDependencies; 
