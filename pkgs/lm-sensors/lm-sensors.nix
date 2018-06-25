dcb18c541fef5a7251bdcaf91951bc587a470d68
Iulian Meghea <iulian.meghea@gmail.com>
2018-06-25 18:22:43 +0300

    Load lm-sensors extra config from /etc/sensors.d

--- pkgs/lm-sensors/lm-sensors.nix
+++ pkgs/lm-sensors/lm-sensors.nix
@@ -8,7 +8,8 @@
         rev = "70f7e0848410b9ca4dde7abff669bbbecbf137e0";
         sha256 = "18896dqjym1jb5vvrdhg2hxbd2q0pnwrxh4jczcax3s0b589z1kj";
       };
-      patches = [];
+      patches = [ ./config.patch ];
+      NIX_CFLAGS_COMPILE = toString (old.NIX_CFLAGS_COMPILE or "") + " -DDEFAULT_CONFIG_DIR=\"/etc/sensors.d\"";
     });
   })];
 }
