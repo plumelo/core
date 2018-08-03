f75225798db89b4b550fc06511a95ec23934f3b0
Unknown <iulian.meghea@gmail.com>
2018-08-03 22:47:50 +0300

    Upgrade lm-sensors

--- pkgs/lm-sensors/lm-sensors.nix
+++ pkgs/lm-sensors/lm-sensors.nix
@@ -5,8 +5,8 @@
       src = fetchFromGitHub {
         owner = "groeck";
         repo = "lm-sensors";
-        rev = "70f7e0848410b9ca4dde7abff669bbbecbf137e0";
-        sha256 = "18896dqjym1jb5vvrdhg2hxbd2q0pnwrxh4jczcax3s0b589z1kj";
+        rev = "dcf23676cc264927ad58ae7960f518689372741a";
+        sha256 = "0d0mcd6fcal9a36i5a40xi1g0p510lvh5bzlq6jdvkyrxa7wnhl2";
       };
       patches = [ ./config.patch ];
       NIX_CFLAGS_COMPILE = toString (old.NIX_CFLAGS_COMPILE or "") + " -DDEFAULT_CONFIG_DIR=\"/etc/sensors.d\"";
