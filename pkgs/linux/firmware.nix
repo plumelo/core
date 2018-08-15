3c29072fec406e342312ecded756590aadda2b7d
Iulian Meghea <iulian.meghea@gmail.com>
2018-08-15 09:41:15 +0300

    Fix linux firmware hashes

--- pkgs/linux/firmware.nix
+++ pkgs/linux/firmware.nix
@@ -3,14 +3,15 @@
   nixpkgs.overlays = [( self: super: { 
     firmwareLinuxNonfree = with super; firmwareLinuxNonfree.overrideAttrs(old: rec {
       name = "firmware-linux-nonfree-${version}";
-      version = "2018-07-28";
+      version = "2018-07-30";
 
       src = fetchgit {
         url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
         rev = "7b5835fd37630d18ac0c755329172f6a17c1af29";
         sha256 = "111pk6867an94gqshbyrbghsn9pbzjhgswdcjwd4z7cl7ikh6q0p";
       };
-    }); 
+      outputHash = "04j8zisycm92jm0w86j8lnx5n7s1z11r1f9kq3p6x7hxikpwfqxx";
+    });
 
     mesa_drivers = (with super; mesa_drivers.overrideAttrs (attrs: rec {
       version = "18.0.5";
@@ -22,7 +23,7 @@
         sha256 = "0szwqjr5x2q4r6gcwb862s18wl4s0z34w4rd0vr7isiasylbp1si";
       };
     })).drivers;
-    
+
     xorg = with super; xorg // {
       xf86videoamdgpu = xorg.xf86videoamdgpu.overrideAttrs (attrs: rec {
         name = "xf86-video-amdgpu-18.0.1";
