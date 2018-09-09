08e9f0608246241c0d46d1ccc77228e396c6bf5e
Meghea Iulian <iulian.meghea@gmail.com>
2018-09-09 09:07:26 +0300

    [firmware] update and cleanup

--- pkgs/linux/firmware.nix
+++ pkgs/linux/firmware.nix
@@ -3,26 +3,25 @@
   nixpkgs.overlays = [( self: super: { 
     firmwareLinuxNonfree = with super; firmwareLinuxNonfree.overrideAttrs(old: rec {
       name = "firmware-linux-nonfree-${version}";
-      version = "2018-08-27";
-
+      version = "2018-09-04";
       src = fetchgit {
         url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-        rev = "fea76a04f25fd0a217c0d566ff5ff8f23ad3e648";
-        sha256 = "1xy1s9vd7jny4hf4f1hzhlbnm0l4pnw7xycky0v6kfwlg5nnjii5";
+        rev = "85c5d90fc155d78531efa5d2b02e92aaef7e4b88";
+        sha256 = "1yjn0s5vkpbvx8wy07wiszg59jp15szz0s69jw3irgbnbk8jgpyv";
       };
-      outputHash = "1p1dkzclj718w7di81s6486dn5zw77c4i2qn63bvp9q4vid452hn";
+      outputHash = "076960qhiz2gdzlnw29fmx3jk71ks4dgf2mfymdaj7d9frnns8mv";
     });
 
-    mesa_drivers = (with super; mesa_drivers.overrideAttrs (attrs: rec {
-      version = "18.0.5";
-      name = "mesa-noglu-${version}";
-      passthru = attrs.passthru // { inherit version; };
+    # mesa_drivers = (with super; mesa_drivers.overrideAttrs (attrs: rec {
+    #   version = "18.0.5";
+    #   name = "mesa-noglu-${version}";
+    #   passthru = attrs.passthru // { inherit version; };
 
-      src =  fetchurl {
-        url = "https://mesa.freedesktop.org/archive/mesa-${version}.tar.xz";
-        sha256 = "0szwqjr5x2q4r6gcwb862s18wl4s0z34w4rd0vr7isiasylbp1si";
-      };
-    })).drivers;
+    #   src =  fetchurl {
+    #     url = "https://mesa.freedesktop.org/archive/mesa-${version}.tar.xz";
+    #     sha256 = "0szwqjr5x2q4r6gcwb862s18wl4s0z34w4rd0vr7isiasylbp1si";
+    #   };
+    # })).drivers;
 
     xorg = with super; xorg // {
       xf86videoamdgpu = xorg.xf86videoamdgpu.overrideAttrs (attrs: rec {
@@ -33,12 +32,13 @@
         };
       });
     };
-    libdrm = with super; libdrm.overrideAttrs (old: rec {
-      name = "libdrm-2.4.94";
-      src = fetchurl {
-        url = "https://dri.freedesktop.org/libdrm/${name}.tar.bz2";
-        sha256 = "1ghn3l1dv1rsp9z6jpmy4ryna1s8rm4xx0ds532041bnlfq5jg5p";
-      };
-    });
+
+    # libdrm = with super; libdrm.overrideAttrs (old: rec {
+    #   name = "libdrm-2.4.94";
+    #   src = fetchurl {
+    #     url = "https://dri.freedesktop.org/libdrm/${name}.tar.bz2";
+    #     sha256 = "1ghn3l1dv1rsp9z6jpmy4ryna1s8rm4xx0ds532041bnlfq5jg5p";
+    #   };
+    # });
   })];
 }
