fdc6e135de060f55299618009294a2b5423e627f
Meghea Iulian <iulian.meghea@gmail.com>
2018-10-17 19:22:07 +0300

    [wine] update wine

--- modules/virtualisation/wine.nix
+++ modules/virtualisation/wine.nix
@@ -2,6 +2,16 @@
 {
   environment.systemPackages = with pkgs; [
     wineStaging
+    ((winetricks.overrideAttrs (old: rec {
+      src = fetchFromGitHub rec {
+        rev = "20180815";
+        sha256 = "0ksz2jkpqq8vnsc511zag9zcx8486hs8mwlmkkygljc8ylb1ibn5";
+        owner = "Winetricks";
+        repo = "winetricks";
+      };
+    })).override {
+      wine = wineStaging;
+    })
   ];
   hardware.opengl.driSupport32Bit = true;
   hardware.pulseaudio.support32Bit = true;
