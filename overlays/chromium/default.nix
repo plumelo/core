0c605c743fc7b64f42d60d483e3668ddc14c3f43
vasile <elsile69@yahoo.com>
2018-10-22 12:27:33 +0300

    Chromium flags

---
+++ overlays/chromium/default.nix
@@ -0,0 +1,8 @@
+self: super: 
+with super; 
+{
+
+  chromium = chromium.override {
+    commandLineArgs = "--enable-native-gpu-memory-buffers --enable-gpu-rasterization --ignore-gpu-blacklist --enable-zero-copy";
+  };
+}
