d39b91be1bf2007b6ba80244457e33f70ddd9051
Meghea Iulian <iulian.meghea@gmail.com>
2018-11-19 07:27:32 +0200

    [skype] upgrade

--- overlays/skype/default.nix
+++ overlays/skype/default.nix
@@ -1,11 +1,11 @@
 self: super: 
 {
   skypeforlinux = with super; skypeforlinux.overrideAttrs (old: rec { 
-    version = "8.32.76.42";
+    version = "8.35.76.30";
 
     src = fetchurl {
       url = "https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_${version}_amd64.deb";
-      sha256 = "1zk7c20hky8va8k135kp1v71gb95fbpxap7brr9zy1il1y7qkj2p";
+      sha256 = "04vp5s55fpcfq3wb4pk3b08dql924w9wn56b859n9n711x3q5762";
     };
   });
 }
