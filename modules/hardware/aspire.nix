1e7f54836f5d9dec39306871c0c8d19ef83efc5b
Meghea Iulian <iulian.meghea@gmail.com>
2018-12-09 18:15:12 +0200

    fix aspire osi

--- modules/hardware/aspire.nix
+++ modules/hardware/aspire.nix
@@ -5,6 +5,8 @@
     kernelParams = [
       "acpi_backlight=vendor"
       "i8042.nopnp"
+      "acpi_osi=!"
+      "acpi_osi=Linux"
       "ivrs_ioapic[4]=00:14.0"
       "ivrs_ioapic[5]=00:00.2"
     ];
