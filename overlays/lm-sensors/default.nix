c07cacc7383d6aa5da7bf9fbad43bf1d9dd6cbe8
Meghea Iulian <iulian.meghea@gmail.com>
2018-12-30 11:19:36 +0200

    [lm-sensors] upgrade to 3.5.0

--- overlays/lm-sensors/default.nix
+++ overlays/lm-sensors/default.nix
@@ -1,12 +1,14 @@
 self: super: {
   lm_sensors = with super; lm_sensors.overrideAttrs(old: rec {
+    version = "3.5.0";
+    name = "lm-sensors-${version}";
     src = fetchFromGitHub {
-      owner = "groeck";
+      owner = "lm-sensors";
       repo = "lm-sensors";
-      rev = "dcf23676cc264927ad58ae7960f518689372741a";
-      sha256 = "0d0mcd6fcal9a36i5a40xi1g0p510lvh5bzlq6jdvkyrxa7wnhl2";
+      rev = "e8afbda10fba571c816abddcb5c8180afc435bba";
+      sha256 = "1mdrnb9r01z1xfdm6dpkywvf9yy9a4yzb59paih9sijwmigv19fj";
     };
-    patches = [ ./config.patch ];
-    NIX_CFLAGS_COMPILE = toString (old.NIX_CFLAGS_COMPILE or "") + " -DDEFAULT_CONFIG_DIR=\"/etc/sensors.d\"";
+    patches = [];
+    # NIX_CFLAGS_COMPILE = toString (old.NIX_CFLAGS_COMPILE or "") + " -DDEFAULT_CONFIG_DIR=\"/etc/sensors.d\"";
   });
 }
