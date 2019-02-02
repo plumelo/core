4f044d89108ebd51a293f8209744e2b68d35c5d7
Meghea Iulian <iulian.meghea@gmail.com>
2019-02-02 10:06:25 +0200

    [A315-41] update kernel flags

--- modules/hardware/aspire.nix
+++ modules/hardware/aspire.nix
@@ -8,7 +8,11 @@
       "acpi_osi=!"
       "acpi_osi=Linux"
       "ivrs_ioapic[4]=00:14.0"
-      "ivrs_ioapic[5]=00:00.2"
+      "ivrs_ioapic[5]e00:00.2"
+      "clocksource=hpet"
+      "iommu=on"
+      "amd_iommu=on"
+      "amdgpu.dc=1"
     ];
     blacklistedKernelModules = [
       "dell-laptop"
