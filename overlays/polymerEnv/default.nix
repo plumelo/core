c1087c6742b58aa7fb4da32b21c02e34ff4bf284
Meghea Iulian <iulian.meghea@gmail.com>
2019-03-10 15:13:28 +0200

    [polymerEnv] fixes sauce-connect

--- overlays/polymerEnv/default.nix
+++ overlays/polymerEnv/default.nix
@@ -4,13 +4,14 @@ self: super:
     name  = "polymer";
     paths = [
       firefox
-      nodejs-10_x 
+      nodejs-10_x
       (writeShellScriptBin "yarn" ''
         export LAUNCHPAD_CHROME=${google-chrome}/bin/google-chrome-stable
         exec -a "$0" ${yarn}/bin/yarn "$@"
       '')
-      (writeShellScriptBin "link-chromedriver" '' 
+      (writeShellScriptBin "link-chromedriver" ''
         ln -sfnv "${chromedriver}/bin/chromedriver" "$(find "./node_modules" -path '**/chromedriver/*-chromedriver')"
+        ln -sfnv "${sauce-connect}/bin/sc" "$(find "./node_modules" -path '**/sauce-connect-launcher/sc/sc-*-linux/bin/sc')"
       '')
     ];
   };
