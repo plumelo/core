9d1b56fc977c0cb2381f7a985d01480ed710e901
Vasile Luta <elsile69@yahoo.com>
2019-04-25 12:25:04 +0300

    [waybar] enable pulseaudio

--- overlays/wayland/default.nix
+++ overlays/wayland/default.nix
@@ -2,7 +2,9 @@ self: super:
 let
   url = "https://github.com/colemickens/nixpkgs-wayland/archive/master.tar.gz";
   waylandPkgs = ((import (builtins.fetchTarball url)) self super).waylandPkgs;
-in
-{
-  inherit (waylandPkgs) waybar redshift-wayland;
+in {
+
+  waybar = super.waybar.override {
+    pulseSupport = true;
+  };
 }
