8480477fe9179fe573b3e6d354c245c499688d69
Vasile Luta <elsile69@yahoo.com>
2019-05-09 14:20:44 +0300

    [tig] upgrade to last commit

--- overlays/tig/default.nix
+++ overlays/tig/default.nix
@@ -4,14 +4,28 @@ with super;
   gitAndTools = gitAndTools // {
     tig = super.symlinkJoin {
 
-    name = "tig-with-config";
+      name = "tig-with-config";
 
-    paths = [gitAndTools.tig];
-    nativeBuildInputs = [ super.makeWrapper ];
+      paths = [
+        (gitAndTools.tig.overrideAttrs(old: rec {
+          pname = "tig";
+          version = "2.4.2";
+          name = "${pname}-${version}";
 
-    postBuild = ''
-      wrapProgram $out/bin/tig --set TIGRC_USER ${ ./tigrc}
-    '';
+          src = fetchFromGitHub {
+            owner = "jonas";
+            repo = pname;
+            rev = "ea43f8be22c7b985c7c752d938579fe14d8e9f08";
+            sha256 = "1x95rvg8m85cda5nl0nwlmkqkar0ahf4sw93km6mfgjhkk5igmrb";
+          };
+        }))
+      ];
+
+      nativeBuildInputs = [ super.makeWrapper ];
+
+      postBuild = ''
+        wrapProgram $out/bin/tig --set TIGRC_USER ${ ./tigrc}
+      '';
     };
   };
 }
