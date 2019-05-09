df42a580673957bf3a865c2907394b8f8e6721db
Meghea Iulian <iulian.meghea@gmail.com>
2019-05-09 09:16:57 +0300

    [lxd] cleanup

--- modules/virtualisation/lxd.nix
+++ modules/virtualisation/lxd.nix
@@ -1,14 +1,4 @@
 { config, lib, pkgs, ... }:
 {
-  # This doesn't seem to work
-  security.apparmor = {
-    enable = true;
-    profiles = [
-      "${pkgs.lxc}/etc/apparmor.d/usr.bin.lxc-start"
-      "${pkgs.lxc}/etc/apparmor.d/lxc-containers"
-    ];
-    packages = [ pkgs.lxc ];
-  };
-
   virtualisation.lxd.enable = true;
 }
