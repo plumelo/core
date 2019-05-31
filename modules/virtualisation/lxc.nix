aff87bd929c76b3ffbb18f577cf850669c0e605b
vasy <elsile69@yahoo.com>
2019-05-31 16:55:25 +0300

    nixos:: refactor lxc module (#25)
    * nixos:: refactor lxc module
    
    * default/lxc-net when lxc.net.enable is true

--- modules/virtualisation/lxc.nix
+++ modules/virtualisation/lxc.nix
@@ -1,55 +1,91 @@
-{ config, lib, pkgs, ... }:
-{
-  environment.systemPackages = with pkgs; [
-    lxc-templates
-  ];
+{ config, options, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.virtualisation.lxc;
+in {
+  options = {
+    virtualisation.lxc.net = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+      };
 
-  system.activationScripts = {
-    lxc = {
-      text = ''
-        mkdir -p /usr/share
-        ln -sfn /run/current-system/sw/share/lxc /usr/share/lxc
-      '';
-      deps = [];
+      domain = mkOption {
+        type = types.string;
+        default = "local";
+      };
+
+      address = mkOption {
+        type = types.string;
+        default = "10.0.3.1";
+
+      };
+
     };
   };
 
-  virtualisation = {
-    lxc = {
-      enable = true;
-      defaultConfig = ''
-        lxc.apparmor.profile = unconfined
-        lxc.net.0.type = veth
-        lxc.net.0.link = lxcbr0
-        lxc.net.0.flags = up
-      '';
-      lxcfs.enable = true;
-    };
-  };
+  config = mkIf cfg.enable ( mkMerge [
+    ({
+      environment.systemPackages = with pkgs; [
+        lxc-templates
+      ];
 
-  environment.etc."default/lxc".text = ''
-    [ ! -f /etc/default/lxc-net ] || . /etc/default/lxc-net
-  '';
+      system.activationScripts = {
+        lxc = {
+          text = ''
+            mkdir -p /usr/share
+            ln -sfn /run/current-system/sw/share/lxc /usr/share/lxc
+          '';
+          deps = [];
+        };
+      };
 
-  environment.etc."default/lxc-net".text = ''
-    USE_LXC_BRIDGE="true"
-    LXC_DOMAIN="local"
-    LXC_ADDR="10.0.3.1"
-  '';
+      virtualisation.lxc = {
+        defaultConfig = ''
+          lxc.apparmor.profile = unconfined
+        '';
+      };
 
-  systemd.services.lxc-net = {
-    after     = [ "network.target" "systemd-resolved.service" ];
-    wantedBy  = [ "multi-user.target" ];
-    path      = with pkgs; [ dnsmasq lxc iproute iptables glibc];
+    })
 
-    serviceConfig = {
-      Type            = "oneshot";
-      RemainAfterExit = "yes";
-      ExecStart       = "${pkgs.lxc}/libexec/lxc/lxc-net start";
-      ExecStop        = "${pkgs.lxc}/libexec/lxc/lxc-net stop";
-    };
-  };
+    (mkIf cfg.net.enable {
+      environment.etc."default/lxc" = {
+        text = ''
+          [ ! -f /etc/default/lxc-net ] || . /etc/default/lxc-net
+        '';
+      };
 
-  environment.pathsToLink = ["/share/lxc"];
+      environment.etc."default/lxc-net" = {
+        text = ''
+          LXC_DOMAIN="${cfg.net.domain}"
+          LXC_ADDR="${cfg.net.address}"
+          USE_LXC_BRIDGE="true"
+        '';
+      };
+
+      virtualisation.lxc = {
+        defaultConfig = ''
+          lxc.net.0.type = veth
+          lxc.net.0.link = lxcbr0
+          lxc.net.0.flags = up
+        '';
+      };
+
+      systemd.services = {
+        lxc-net = {
+          after     = [ "network.target" "systemd-resolved.service" ];
+          wantedBy  = [ "multi-user.target" ];
+          path      = with pkgs; [ dnsmasq lxc iproute iptables glibc];
+
+          serviceConfig = {
+            Type            = "oneshot";
+            RemainAfterExit = "yes";
+            ExecStart       = "${pkgs.lxc}/libexec/lxc/lxc-net start";
+            ExecStop        = "${pkgs.lxc}/libexec/lxc/lxc-net stop";
+          };
+        };
+      };
+    })
+
+  ]);
 }
-
