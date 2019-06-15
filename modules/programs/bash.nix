b1be8fe214d23c4029dfa783a67210e5a9810a14
Meghea Iulian <iulian.meghea@gmail.com>
2019-06-15 18:53:45 +0300

    [bash] add hstr and fzf

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -1,6 +1,10 @@
 { config, options, lib, pkgs, ... }:
 {
   programs.bash = {
+    shellAliases= {
+      hh="${pkgs.hstr}/bin/hh";
+      "~"="cd ~";
+    };
     promptInit = ''
       PS='\[\e[32m\]\w\[\e[m\]$(${pkgs.bashPackages.vcprompt}/bin/vcprompt --minimal) \[\e[35m\]$\[\e[m\] '
       function prompt_command() {
@@ -15,6 +19,21 @@
     '';
     interactiveShellInit= ''
       export EDITOR=nvim
+
+      shopt -s histappend
+      export HISTFILESIZE=10000
+      export HISTSIZE=''${HISTFILESIZE}
+
+      export HH_CONFIG=hicolor
+      export HISTCONTROL=ignorespace
+      export PROMPT_COMMAND="history -a; history -n; ''${PROMPT_COMMAND}"   # mem/file sync
+
+      source ${pkgs.fzf}/share/fzf/completion.bash
+      source ${pkgs.fzf}/share/fzf/key-bindings.bash
+
+      bind '"\C-r": "\C-a hh -- \C-j"';
+      bind '"\C-xk": "\C-a hh -k \C-j"'
+
     '';
   };
 }
