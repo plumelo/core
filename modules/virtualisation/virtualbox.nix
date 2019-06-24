6db0c591a21297f32a0e38167174afd4b3bf0816
vasy <elsile69@yahoo.com>
2019-06-24 15:53:03 +0300

    virtualbox module (#33)

--- modules/virtualisation/virtualbox.nix
+++ modules/virtualisation/virtualbox.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 with lib;
 let
-  cfg = config.virtualbox.host;
+  cfg = config.virtualisation.virtualbox.host;
 in {
-  config = mkIf cfg.host.enable {
+  config = mkIf cfg.enable {
     virtualisation.virtualbox.host = {
       enableExtensionPack = true;
     };
