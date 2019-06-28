34e0d9786193090e702c0ed52cb39b9cb0b3e49e
vasile luta <elsile69@yahoo.com>
2019-06-28 13:53:55 +0300

    module-ssh:: add persistentHosts option

--- modules/programs/ssh/default.nix
+++ modules/programs/ssh/default.nix
@@ -8,11 +8,21 @@ let
     exec ${askPassword} 2>/tmp/ask.log
   '';
 in {
+  options = {
+    programs.ssh = {
+      persistentHosts = lib.mkOption {
+        type = lib.types.listOf;
+        default = [];
+      };
+    };
+  };
+
   programs.ssh.startAgent = true;
   systemd.user.services.ssh-agent.environment.SSH_ASKPASS = lib.mkForce askPasswordWrapper;
   environment.variables.SSH_ASKPASS = lib.mkForce askPassword;
   programs.ssh.extraConfig = ''
     Host github.com
+    HostName ${cfg.persistentHosts}
     ControlMaster auto
     ControlPath ~/.ssh/sockets-%r@%h-%p
     ControlPersist 600
