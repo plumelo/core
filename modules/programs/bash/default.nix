860d6882a182663c8b08b29b9a7b869240ca1604
vasile luta <elsile69@yahoo.com>
2019-07-11 15:42:58 +0300

    module bash:: custom configs

--- modules/programs/bash/default.nix
+++ modules/programs/bash/default.nix
@@ -7,44 +7,37 @@
       grep="grep --color=auto";
     };
     promptInit = ''
-      NL='
-      '
       GIT_PS1_SHOWUNTRACKEDFILES=1
       GIT_PS1_SHOWUPSTREAM="auto"
       GIT_PS1_SHOWCOLORHINTS=1
       GIT_PS1_SHOWDIRTYSTATE=1
-      VIRTUAL_ENV_DISABLE_PROMPT=1
       bold=$(tput bold)
       red=$(tput setaf 1)
       yellow=$(tput setaf 3)
       cyan=$(tput setaf 6)
       magenta=$(tput setaf 5)
       reset=$(tput sgr0)
-      set_active_venv() {
-        export ACTIVE_VENV=""
-        if [ "$VIRTUAL_ENV" != "" ]; then
-          export ACTIVE_VENV="(`basename \"$VIRTUAL_ENV\"`)"
-        fi
+      prompt_command(){
+        local retval="''${?#0}"
+        history -n; history -w; history -c; history -r
+        local prompt="\n"
+        local uid=""
+        local job="\j"
+
+        [ $(id -u) -eq 0 ]  && uid+="$jobs\n\[$red\]#\[$reset\]" || uid+="$jobs\n\[$magenta\]❯\[$reset\]"
+        if [ -n "$(jobs -p)" ]; then uid+="$job"; fi
+        if [ -n "$retval" ]; then prompt+="\[$red\]$retval\[$reset\] " ;fi
+        if [ -n "$IN_NIX_SHELL" ]; then prompt+="\[$cyan\] \[$reset\] "; fi
+        prompt+="\[$bold\]\[$yellow\]\w\[$reset\]"
+
+        __git_ps1 "$prompt" "$uid "
+
       }
-      active_nix_shell() {
-        if [ -n "$IN_NIX_SHELL" ]; then
-          export NSHELL="  "
-        fi
-      }
-      PROMPT_COMMAND='\
-        set_active_venv; active_nix_shell; __git_ps1 \
-        "''${NL}\[$yellow\]''${ACTIVE_VENV}''${NSHELL}\[$reset\]\[$red\]\''${?#0}\[$reset\]\[$bold\]\[$cyan\]\w\[$reset\]" \
-        "\[$magenta\]''${NL}❯\[$reset\] "'
-      alias hfix='history -n && history | sort -k2 -k1nr | uniq -f1 | sort -n | cut -c8- > ~/.tmp$$ && history -c && history -r ~/.tmp$$ && history -w && rm ~/.tmp$$'
-      PROMPT_COMMAND="hfix; $PROMPT_COMMAND"
+      PROMPT_COMMAND=prompt_command
+      PROMPT_DIRTRIM=3
     '';
     interactiveShellInit= ''
-      shopt -s checkwinsize
-      PROMPT_DIRTRIM=3
-      bind Space:magic-space
-      shopt -s globstar 2> /dev/null
-      shopt -s nocaseglob;
-
+      set -o notify
       bind "set completion-ignore-case on"
       bind "set completion-map-case on"
       bind "set show-all-if-ambiguous on"
@@ -53,49 +46,36 @@
       bind "set colored-stats on"
       bind "set skip-completed-text on"
       bind "set visible-stats on"
-      bind "set mark-symlinked-directories on"
       bind "set page-completions off"
-      bind "set skip-completed-text on"
       bind "set bell-style none"
       bind '"\t": menu-complete'
       bind '"\e[Z": menu-complete-backward'
       bind '"\e[P": delete-char'
       bind '"\e[A": history-search-backward'
       bind '"\e[B": history-search-forward'
-      bind '"\e[C": forward-char'
-      bind '"\e[D": backward-char'
       bind '"\C-h": backward-kill-word'
-      bind '"\e[1;5C": forward-word'
-      bind '"\e[1;5D": backward-word'
       bind '"\e[M": kill-word'
+      bind Space:magic-space
+      shopt -s checkwinsize
+      shopt -s globstar 2> /dev/null
+      shopt -s nocaseglob;
       shopt -s autocd 2> /dev/null
       shopt -s dirspell 2> /dev/null
       shopt -s cdspell 2> /dev/null
-
-      set meta-flag on
-      set input-meta on
-      set output-meta on
-      set convert-meta off
-
-      LESS='-XFr'
-
       shopt -s histappend
       shopt -s histreedit
       shopt -s histverify
       shopt -s cmdhist
       shopt -s lithist
-
       HISTFILESIZE=-1
       HISTSIZE=''${HISTFILESIZE}
       HH_CONFIG=hicolor
       HISTCONTROL=ignoreboth:erasedups
-
       eval `${pkgs.coreutils}/bin/dircolors "${./dircolors}"`
       source ${pkgs.fzf}/share/fzf/completion.bash
       source ${pkgs.fzf}/share/fzf/key-bindings.bash
       bind '"\C-r": "\C-a hh -- \C-j"';
       bind '"\C-xk": "\C-a hh -k \C-j"'
-
       stty -ixon
     '';
   };
