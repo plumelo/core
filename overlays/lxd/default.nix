99f1c41d3695055ff9d6113040c15e64b231fa1d
Meghea Iulian <iulian.meghea@gmail.com>
2019-07-11 09:48:41 +0300

    lxc-templates: 3.0.3 -> 3.0.4

--- overlays/lxd/default.nix
+++ overlays/lxd/default.nix
@@ -1,20 +1,21 @@
 self: super:
+with super;
 {
-  lxc-templates = with super; stdenv.mkDerivation rec {
+  lxc-templates = stdenv.mkDerivation rec {
     name = "lxc-templates-${version}";
-    version = "3.0.3";
+    version = "3.0.4";
     src = fetchFromGitHub {
       owner = "lxc";
       repo = "lxc-templates";
       rev = "lxc-templates-${version}";
-      sha256 = "15ywjgaimvxwc7xk5f4s1k7d384vfs12bc1jywhxahgirjjqgw2l";
+      sha256 = "1cgfxi6j28d92v7wl37wz17dw6rw1sg1j72pwh32h0nik5gdd1y5";
     };
     preConfigure = ''
-        for file in $(find ./config -type f -name  "*.conf.in"); do
-          substituteInPlace $file \
-            --replace "@LXCTEMPLATECONFIG@/common.conf" ${lxc}/share/lxc/config/common.conf \
-            --replace "@LXCTEMPLATECONFIG@/userns.conf" ${lxc}/share/lxc/config/userns.conf
-        done
+      for file in $(find ./config -type f -name  "*.conf.in"); do
+        substituteInPlace $file \
+          --replace "@LXCTEMPLATECONFIG@/common.conf" ${lxc}/share/lxc/config/common.conf \
+          --replace "@LXCTEMPLATECONFIG@/userns.conf" ${lxc}/share/lxc/config/userns.conf
+      done
     '';
     postInstall = ''
         rm -rf $out/var
