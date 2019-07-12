422b694889a8eab2f2686da2ef208946370fabae
vasy <elsile69@yahoo.com>
2019-07-12 20:21:47 +0300

    [bash] refactor prompt (#38)
    * module bash:: custom configs
    
    * module bash:: fixes

--- modules/programs/bash/default.nix
+++ modules/programs/bash/default.nix
@@ -7,43 +7,47 @@
       grep="grep --color=auto";
     };
     promptInit = ''
-      NL='
-      '
       GIT_PS1_SHOWUNTRACKEDFILES=1
       GIT_PS1_SHOWUPSTREAM="auto"
       GIT_PS1_SHOWCOLORHINTS=1
       GIT_PS1_SHOWDIRTYSTATE=1
-      VIRTUAL_ENV_DISABLE_PROMPT=1
       bold=$(tput bold)
       red=$(tput setaf 1)
       yellow=$(tput setaf 3)
       cyan=$(tput setaf 6)
       magenta=$(tput setaf 5)
       reset=$(tput sgr0)
-      set_active_venv() {
-        export ACTIVE_VENV=""
-        if [ "$VIRTUAL_ENV" != "" ]; then
-          export ACTIVE_VENV="(`basename \"$VIRTUAL_ENV\"`)"
-        fi
+      prompt_command(){
+        local retval="''${?#0}"
+        local prompt="\n"
+        local prompt_end=""
+        local jobsval=`jobs -p | wc -l`
+
+        [ -n "$retval" ] && prompt+="$red$retval$reset "
+        [ -n "$IN_NIX_SHELL" ] && prompt+="$cyan $reset "
+        prompt+="$bold$yellow\w$reset"
+
+        [ $jobsval -ne 0 ] && prompt_end+=" $jobsval"
+        prompt_end+="\n"
+        [ $(id -u) -eq 0 ] && prompt_end+="\[$red\]#\[$reset\]" || prompt_end+="\[$magenta\]❯\[$reset\]"
+
+        __git_ps1 "$prompt" "$prompt_end "
+        history -n; history -w; history -c; history -r
       }
-      active_nix_shell() {
-        if [ -n "$IN_NIX_SHELL" ]; then
-          export NSHELL="  "
-        fi
-      }
-      PROMPT_COMMAND='\
-        set_active_venv; active_nix_shell; __git_ps1 \
-        "''${NL}\[$yellow\]''${ACTIVE_VENV}''${NSHELL}\[$reset\]\[$red\]\''${?#0}\[$reset\]\[$bold\]\[$cyan\]\w\[$reset\]" \
-        "\[$magenta\]''${NL}❯\[$reset\] "'
-      alias hfix='history -n && history | sort -k2 -k1nr | uniq -f1 | sort -n | cut -c8- > ~/.tmp$$ && history -c && history -r ~/.tmp$$ && history -w && rm ~/.tmp$$'
-      PROMPT_COMMAND="hfix; $PROMPT_COMMAND"
+      PROMPT_COMMAND=prompt_command
     '';
     interactiveShellInit= ''
-      shopt -s checkwinsize
       PROMPT_DIRTRIM=3
-      bind Space:magic-space
+      set -o notify
+
+      shopt -s checkwinsize
       shopt -s globstar 2> /dev/null
       shopt -s nocaseglob;
+      shopt -s autocd 2> /dev/null
+      shopt -s dirspell 2> /dev/null
+      shopt -s cdspell 2> /dev/null
+      shopt -s histappend
+      shopt -s cmdhist
 
       bind "set completion-ignore-case on"
       bind "set completion-map-case on"
@@ -51,51 +55,28 @@
       bind "set menu-complete-display-prefix on"
       bind "set mark-symlinked-directories on"
       bind "set colored-stats on"
-      bind "set skip-completed-text on"
       bind "set visible-stats on"
-      bind "set mark-symlinked-directories on"
       bind "set page-completions off"
       bind "set skip-completed-text on"
       bind "set bell-style none"
+
       bind '"\t": menu-complete'
       bind '"\e[Z": menu-complete-backward'
-      bind '"\e[P": delete-char'
       bind '"\e[A": history-search-backward'
       bind '"\e[B": history-search-forward'
-      bind '"\e[C": forward-char'
-      bind '"\e[D": backward-char'
-      bind '"\C-h": backward-kill-word'
-      bind '"\e[1;5C": forward-word'
-      bind '"\e[1;5D": backward-word'
       bind '"\e[M": kill-word'
-      shopt -s autocd 2> /dev/null
-      shopt -s dirspell 2> /dev/null
-      shopt -s cdspell 2> /dev/null
-
-      set meta-flag on
-      set input-meta on
-      set output-meta on
-      set convert-meta off
-
-      LESS='-XFr'
-
-      shopt -s histappend
-      shopt -s histreedit
-      shopt -s histverify
-      shopt -s cmdhist
-      shopt -s lithist
+      bind '"\C-h": backward-kill-word'
 
       HISTFILESIZE=-1
       HISTSIZE=''${HISTFILESIZE}
-      HH_CONFIG=hicolor
-      HISTCONTROL=ignoreboth:erasedups
+      HISTCONTROL="erasedups:ignoreboth"
+      HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:?:??"
 
       eval `${pkgs.coreutils}/bin/dircolors "${./dircolors}"`
       source ${pkgs.fzf}/share/fzf/completion.bash
       source ${pkgs.fzf}/share/fzf/key-bindings.bash
       bind '"\C-r": "\C-a hh -- \C-j"';
       bind '"\C-xk": "\C-a hh -k \C-j"'
-
       stty -ixon
     '';
   };
