0d6e8c9c589d9a6eb3bcb4f1fda040bb7bd1ab8b
Meghea Iulian <iulian.meghea@gmail.com>
2019-07-23 10:30:24 +0300

    reactEnv: refactor with mkShell

--- overlays/reactEnv/default.nix
+++ overlays/reactEnv/default.nix
@@ -1,11 +1,12 @@
 self: super:
-{
-  react = with super; buildEnv {
-    name  = "react";
-    paths = [
-      nodejs-10_x
-      (yarn.override { nodejs = nodejs-10_x; })
-      graphviz
-    ];
+with super;
+let yarn-10_x = super.yarn.override { nodejs = nodejs-10_x; };
+in {
+  react = mkShell {
+    inputsFrom = [ yarn-10_x ];
+    buildInputs = [ yarn-10_x graphviz ];
+    shellHook = ''
+      export XDG_DATA_DIRS=${self.yarn_completions}/share/:$XDG_DATA_DIRS
+    '';
   };
 }
--- overlays/yarn/default.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-with super;
-{
-  yarn = yarn.override {
-    nodejs = nodejs-10_x;
-  };
-}
