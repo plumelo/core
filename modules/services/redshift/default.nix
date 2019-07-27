ca026781f767210b0d8f90afde512fa1aa968224
Meghea Iulian <iulian.meghea@gmail.com>
2019-07-27 15:44:09 +0300

    services.redshift: use redshift-wlroot

--- modules/services/redshift/default.nix
+++ modules/services/redshift/default.nix
@@ -1,7 +1,13 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, ... }:
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/matthias-t/nixpkgs/archive/master.tar.gz";
+    sha256 = "07wprqw6pxn4zggnj22r1qd3wsqr5clprb891gh9wxc839z6v13g";
+  }) { };
+in {
   services.redshift = {
     enable = true;
-    package = pkgs.redshift-wayland;
+    package = pkgs.redshift-wlroots;
     latitude = "47.15";
     longitude = "27.59";
   };
