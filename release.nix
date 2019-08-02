3be6d0a5e28fa270ca89c71bc29bb58f9d2622e3
Meghea Iulian <iulian.meghea@gmail.com>
2019-08-02 10:10:03 +0300

    nixpkgs: upgrade

--- release.nix
+++ release.nix
@@ -1,5 +1,5 @@
 # https://howoldis.herokuapp.com
 (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/42c3eefd778e37c8602f45cdef89fe001f108aad.tar.gz";
-  sha256 = "1k92nhlhhyxdicb1zk1n2d6ikc8hsfk5scs5bms91bivh8j36spy";
+  url = "https://github.com/NixOS/nixpkgs/archive/96d0fae21ed723e1e96ef65bcffd98b722493c46.tar.gz";
+  sha256 = "0xpv59l7lmpdfmcllrg2idhxmhrqa6frbckrpp4dc8wildzvkzx7";
 })
