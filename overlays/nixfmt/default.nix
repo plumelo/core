96daa99920be9fd5632a7762325207df73ee9a98
Meghea Iulian <iulian.meghea@gmail.com>
2019-08-02 10:06:42 +0300

    nixfmt: upgrade

--- overlays/nixfmt/default.nix
+++ overlays/nixfmt/default.nix
@@ -1,7 +1,10 @@
 self: super: {
   nixfmt = (import (fetchTarball {
     url =
-    "https://github.com/serokell/nixfmt/archive/a5c055e95dfb560e39c3adacc79757022d305d9c.tar.gz";
-    sha256 = "1ippahc85i8pz1yyf7bdfycrqkaqpdanv9nq5rn8dz9l2qsvn2px";
-  })) { };
+    "https://github.com/serokell/nixfmt/archive/af09262aa7d08d2b0905b11d2e8958889aadcf46.tar.gz";
+    sha256 = "0njgjv6syv3sk97v8kq0cb4mhgrb7nag2shsj7rphs6h5b7k9nbx";
+  })) {
+    pkgs = super;
+    installOnly = true;
+  };
 }
