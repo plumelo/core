f6260950285b7a963a12c8f1916df3823e83f1d7
Meghea Iulian <iulian.meghea@gmail.com>
2019-08-11 11:46:45 +0300

    ansible-complation: init

---
+++ overlays/ansible-completion/default.nix
@@ -0,0 +1,24 @@
+self: super:
+with super; {
+ ansible-completion = stdenv.mkDerivation rec {
+    name = "yarn-completion";
+    version = "0.0.1";
+
+    src = fetchFromGitHub {
+      owner = "dysosmus";
+      repo = "ansible-completion";
+      rev = "7254afb781e5782ccbe78ba70df1e4ed845deef7";
+      sha256 = "12zxv953q0hwddkc2sfxp6cx9c7ld2qcssv3qvfv17hw8f99wrik";
+    };
+
+    buildPhase = "";
+    installPhase = ''
+      completions="$out/share/bash-completion/completions"
+      mkdir -p $completions
+      for file in *-completion.bash
+      do
+        cp -av "$file" "$completions/''${file/-completion.bash}".bash
+      done
+    '';
+  };
+}
