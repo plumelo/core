9ce357d2a5187d43f68737fee5a8a56ac0738d4d
vasy <elsile69@yahoo.com>
2019-11-09 18:31:16 +0200

    tmux: nord theme (#50)

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -1,6 +1,5 @@
 { config, options, lib, pkgs, ... }:
-with pkgs.tmuxPlugins;
-{
+with pkgs.tmuxPlugins; {
   programs.tmux = {
     enable = true;
     baseIndex = 1;
@@ -26,20 +25,53 @@ with pkgs.tmuxPlugins;
       set -g prefix C-Space
       bind C-Space send-prefix
 
-      set -g @theme "one"
-      set -g @theme-background "dark"
+      # Default colors
+      set-option -g status-style fg=white,bg=black
 
-      set -g status-left-length 30
-      set -g status-right '#{prefix_highlight}'
+      # Status
+      set -g status-interval 1
+
+      # Window titles
+      set-window-option -g window-status-style fg=default,bg=default
+      set-window-option -g window-status-current-style fg=white,bold,bg=default
+      set-window-option -g window-status-activity-style fg=default,noreverse,bg=default
+
+      # Pane borders
+      set-option -g pane-border-style fg=colour240,bg=default
+      set-option -g pane-active-border-style fg=colour76,bg=default
+
+      # Message text
+      set-option -g message-style fg=colour76,bg=default
+      set-option -g message-command-style fg=colour75,bg=default
+
+      # Bars
+      set -g status-left "#[fg=black,bg=blue,bold] #S #[fg=blue,bg=black,nobold,noitalics,nounderscore]"
+      set -g status-right "#{prefix_highlight}#[fg=brightblack,bg=black,nobold,noitalics,nounderscore]#[fg=cyan,bg=brightblack] #[fg=black,bg=cyan,bold] #H "
+
+      # Windows
+      set -g window-status-format "#[fg=black,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#I #[fg=white,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#W #F #[fg=brightblack,bg=black,nobold,noitalics,nounderscore]"
+      set -g window-status-current-format "#[fg=black,bg=cyan,nobold,noitalics,nounderscore] #[fg=black,bg=cyan]#I #[fg=black,bg=cyan,nobold,noitalics,nounderscore] #[fg=black,bg=cyan]#W #F #[fg=cyan,bg=black,nobold,noitalics,nounderscore]"
+      set -g window-status-separator ""
+
+      # Panes
+      set -g pane-border-style "bg=black fg=black"
+      set -g pane-active-border-style "bg=black fg=brightblack"
+      # set -g display-panes-colour black
+      # set -g display-panes-active-colour brightblack
+
+      # tmux-prefix-highlight
+      set -g @prefix_highlight_output_prefix "#[fg=brightcyan]#[bg=black]#[nobold]#[noitalics]#[nounderscore]#[bg=brightcyan]#[fg=black]"
+      set -g @prefix_highlight_output_suffix ""
+      set -g @prefix_highlight_copy_mode_attr "fg=brightcyan,bg=black,bold"
 
       set -g @resurrect-processes '\
         "~nvim->nvim" \
         "~tig->tig" \
       '
+
       run-shell ${pain-control.rtp}
       run-shell ${sensible.rtp}
       run-shell ${resurrect.rtp}
-      run-shell ${tmux-theme.rtp}
       run-shell ${prefix-highlight.rtp}
     '';
   };
