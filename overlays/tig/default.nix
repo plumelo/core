e9ee2149bee48d2199bccc2995836ca901c5b048
vasile luta <elsile69@yahoo.com>
2019-11-11 13:06:08 +0200

    tig: update to latest commit and to nord theme

--- overlays/tig/default.nix
+++ overlays/tig/default.nix
@@ -1,17 +1,30 @@
 self: super:
-with super;
-{
+with super; {
   gitAndTools = gitAndTools // {
     tig = super.symlinkJoin {
 
-    name = "tig-with-config";
+      name = "tig-with-config";
 
-    paths = [gitAndTools.tig];
-    nativeBuildInputs = [ super.makeWrapper ];
+      paths = [
+        (gitAndTools.tig.overrideAttrs (old: rec {
+          pname = "tig";
+          version = "2.4.2";
+          name = "${pname}-${version}";
 
-    postBuild = ''
-      wrapProgram $out/bin/tig --set TIGRC_USER ${ ./tigrc}
-    '';
+          src = fetchFromGitHub {
+            owner = "jonas";
+            repo = pname;
+            rev = "36ced9aff39fcbe13e08cc4b63ab2afc84f5bc6f";
+            sha256 = "1dg0a37xr61d7myc5pci3ag8h54wdv3vwqwhlakxg1y137acfc28";
+          };
+        }))
+      ];
+
+      nativeBuildInputs = [ super.makeWrapper ];
+
+      postBuild = ''
+        wrapProgram $out/bin/tig --set TIGRC_USER ${./tigrc}
+      '';
     };
   };
 }
