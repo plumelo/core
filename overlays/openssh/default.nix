1e0a3b9b49f4272e88eb2d115ee3d0dd7d51f8ca
Meghea Iulian <iulian.meghea@gmail.com>
2019-12-11 13:39:40 +0200

    gnome-ssh-askpass3: use openssh src

--- overlays/openssh/default.nix
+++ overlays/openssh/default.nix
@@ -1,33 +1,18 @@
 self: super:
-with super;
-{
+with super; {
   gnome-ssh-askpass3 = stdenv.mkDerivation rec {
-    src = fetchurl {
-      url = "mirror://openbsd/OpenSSH/portable/${openssh.name}.tar.gz";
-      sha256 = "1b8sy6v0b8v4ggmknwcqx3y1rjcpsll0f1f8f4vyv11x4ni3njvb";
-    };
-  name = "gnome-ssh-askpass3-${version}";
-  version = "0.0.1";
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [gnome3.gtk];
-  sourceRoot = "openssh-7.9p1/contrib";
-  buildPhase = ''
-    make gnome-ssh-askpass3
-  '';
-  installPhase = ''
-    mkdir -p $out/bin;
-    mv gnome-ssh-askpass3 $out/bin
+    src = openssh.src;
+    name = "gnome-ssh-askpass3-${version}";
+    version = "0.0.1";
+    nativeBuildInputs = [ pkgconfig ];
+    buildInputs = [ gnome3.gtk ];
+    sourceRoot = "openssh-${openssh.version}/contrib";
+    buildPhase = ''
+      make gnome-ssh-askpass3
     '';
-};
-  # openssh.overrideAttrs (attrs: rec {
-  #   native
-  #   postBuild = ''
-  #     pushd contrib
-  #     make gnome-ssh-askpass3
-  #     popd
-  #     '';
-  #   postInstall = attrs.postInstall + ''
-  #     ls -al contrib
-  #   '';
-  # });
+    installPhase = ''
+      mkdir -p $out/bin;
+      mv gnome-ssh-askpass3 $out/bin
+    '';
+  };
 }
