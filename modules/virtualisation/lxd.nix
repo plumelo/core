9f0f0a49f3c2663cc6b369ed3042f1c21b2c6010
Meghea Iulian <iulian.meghea@gmail.com>
2019-12-17 13:02:30 +0200

    lxd: enable recommended sysctl settings

--- modules/virtualisation/lxd.nix
+++ modules/virtualisation/lxd.nix
@@ -3,10 +3,11 @@ with lib;
 let cfg = config.virtualisation.lxd;
 in {
   config =
-  mkIf (cfg.enable && (config.networking.networkmanager.dns == "dnsmasq")) ({
-    environment.etc."NetworkManager/dnsmasq.d/10-dns-lxd.conf".text = ''
-      server=/lxd/10.0.4.1
-    '';
-    users.defaultUser.extraGroups = [ "lxd" ];
-  });
+    mkIf (cfg.enable && (config.networking.networkmanager.dns == "dnsmasq")) ({
+      environment.etc."NetworkManager/dnsmasq.d/10-dns-lxd.conf".text = ''
+        server=/lxd/10.0.4.1
+      '';
+      users.defaultUser.extraGroups = [ "lxd" ];
+      virtualisation.lxd.recommendedSysctlSettings = true;
+    });
 }
