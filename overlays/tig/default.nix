e74d79ce7bf3e85a80e056c422b8b40caaf17d18
Meghea Iulian <iulian.meghea@gmail.com>
2019-12-20 07:06:04 +0200

    tig: use upstream

--- overlays/tig/default.nix
+++ overlays/tig/default.nix
@@ -5,20 +5,7 @@ with super; {
 
       name = "tig-with-config";
 
-      paths = [
-        (gitAndTools.tig.overrideAttrs (old: rec {
-          pname = "tig";
-          version = "2.4.2";
-          name = "${pname}-${version}";
-
-          src = fetchFromGitHub {
-            owner = "jonas";
-            repo = pname;
-            rev = "6642c11425a2d7432d968146025b6d9a96db5d64";
-            sha256 = "0pm89f32cxmdqkd9r26z6fmsz41wpgkmgbk7aasq3pwj5gmrrpvc";
-          };
-        }))
-      ];
+      paths = [ gitAndTools.tig ];
 
       nativeBuildInputs = [ super.makeWrapper ];
 
