9d03b8af635029906015008f8b5eea7245c3eda7
Meghea Iulian <iulian.meghea@gmail.com>
2019-12-30 07:14:53 +0200

    console: upgrade settings

--- modules/base.nix
+++ modules/base.nix
@@ -19,12 +19,12 @@
 
   system.stateVersion = "20.03";
 
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
+  i18n = { defaultLocale = "en_US.UTF-8"; };
 
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
   time.timeZone = "Europe/Bucharest";
 
   hardware = {
