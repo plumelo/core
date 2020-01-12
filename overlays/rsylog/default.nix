a39c52e7620127cb16abde9aed1a9cf362787fd0
Meghea Iulian <iulian.meghea@gmail.com>
2020-01-12 09:12:40 +0200

    rsyslog: remove overlay

--- overlays/rsylog/default.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-with super; {
-  rsyslog = rsyslog.overrideAttrs (old: rec {
-    configureFlags = old.configureFlags ++ [ "--enable-mmdblookup" ];
-    buildInputs = old.buildInputs ++ [ libmaxminddb ];
-  });
-}
