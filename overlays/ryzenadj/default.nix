4a6f1d8ebf56e082364335b1411f3e95bf6147eb
Meghea Iulian <iulian.meghea@gmail.com>
2020-02-17 16:10:38 +0200

    ryzenadj: init

---
+++ overlays/ryzenadj/default.nix
@@ -0,0 +1,22 @@
+self: super:
+with super; {
+  ryzenadj = stdenv.mkDerivation rec {
+    name = "RyzenAdj";
+    version = "0.5.3-unstable";
+
+    src = fetchFromGitHub {
+      rev = "82afad3";
+      owner = "FlyGoat";
+      repo = name;
+      sha256 = "1rdmhx0vsyzv2xj2snjb9q51y2rcglwfswmx8lcah3jw8l0x5aa5";
+    };
+    nativeBuildInputs = [ cmake gcc ];
+    buildInputs = [ pciutils ];
+
+    installPhase = ''
+      mkdir -p $out/{bin,lib}
+      cp -av ryzenadj $out/bin
+      cp -av libryzenadj.so $out/lib
+    '';
+  };
+}
