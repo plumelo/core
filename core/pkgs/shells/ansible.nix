043b5906beb3589167c5c29d5fcea6694f52e5f2
Meghea Iulian <iulian.meghea@gmail.com>
2020-04-02 11:31:33 +0300

    cleanup shells

--- core/pkgs/shells/ansible.nix
+++
@@ -1,8 +0,0 @@
-{ mkShell, ansible, ansible-completion }:
-
-mkShell {
-  buildInputs = [ ansible ];
-  shellHook = ''
-    export XDG_DATA_DIRS=${ansible-completion}/share/:$XDG_DATA_DIRS
-  '';
-}
