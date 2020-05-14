0994a1d7ef851df7017ca81deff41a820d9afd69
Meghea Iulian <iulian.meghea@gmail.com>
2020-05-14 11:19:21 +0300

    chore(nixpkgs): upgrade

--- core/patches.nix
+++ core/patches.nix
@@ -1,4 +1,4 @@
-{ writeText }: [
+{ writeText, fetchpatch }: [
   (
     writeText "nixos-overlays-fix.patch" ''
       diff --git a/nixos/modules/misc/nixpkgs.nix b/nixos/modules/misc/nixpkgs.nix
@@ -18,5 +18,13 @@
 
        in
     ''
+  )
+
+  (
+    fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/87797.diff";
+      sha256 = "14imhhaazf8vghdq0w2dwq0g4jxa3ljfph9aw6dc148r7rjmn7pj";
+
+    }
   )
 ]
--- core/release.nix
+++ core/release.nix
@@ -1,5 +1,5 @@
 # https://howoldis.herokuapp.com
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/b690b78b66007df78d40c7d5f5c77bb06ad32faa.tar.gz";
-  sha256 = "0zibvxsk26y76p71b5vzd0r6m63qbi7k621dj6p5n8bqhh6dq31d";
+  url = "https://github.com/NixOS/nixpkgs/archive/9a29fe5.tar.gz";
+  sha256 = "0m2qxkk0npmfk0mihgnxmizgai0qv6hpdxrxgfj852k1cas6373k";
 }
