d3d4ae99a12be29375a2d17268f3c471de4125af
Meghea Iulian <iulian.meghea@gmail.com>
2020-05-29 08:36:43 +0300

    chore(nixpkgs): upgrade

--- core/release.nix
+++ core/release.nix
@@ -1,5 +1,5 @@
 # https://howoldis.herokuapp.com
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/0f5ce2f.tar.gz";
-  sha256 = "0nkk492aa7pr0d30vv1aw192wc16wpa1j02925pldc09s9m9i0r3";
+  url = "https://github.com/NixOS/nixpkgs/archive/46f975f.tar.gz";
+  sha256 = "11f8jli3lhc4hzfy7fjwpks3f3p2vpnkz8lrkcsr4aglrd0n3mjv";
 }
