d93686018a35b878c55af0c0eb275f63308c67de
Meghea Iulian <iulian.meghea@gmail.com>
2020-12-29 21:26:26 +0200

    fix: remove default options

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -1,12 +1,5 @@
 { lib, pkgs, ... }: {
 
-  i18n = { defaultLocale = "en_US.UTF-8"; };
-
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
-
   time.timeZone = "Europe/Bucharest";
 
   location = {
