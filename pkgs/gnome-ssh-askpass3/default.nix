a4ca6a718150c1ab5053ebe9586256abd76f98d1
Meghea Iulian <iulian.meghea@gmail.com>
2022-01-18 23:07:25 +0200

    chore(deps): upgrade

--- pkgs/gnome-ssh-askpass3/default.nix
+++ pkgs/gnome-ssh-askpass3/default.nix
@@ -1,11 +1,11 @@
-{ stdenv, pkgconfig, openssh, gtk }:
+{ stdenv, pkgconfig, openssh, gtk3 }:
 
 stdenv.mkDerivation rec {
   src = openssh.src;
   name = "gnome-ssh-askpass3-${version}";
   version = "0.0.1";
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ gtk ];
+  buildInputs = [ gtk3 ];
   sourceRoot = "openssh-${openssh.version}/contrib";
   buildPhase = ''
     make gnome-ssh-askpass3
