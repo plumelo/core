c0ea160d79948107e14e12a72034c8076ec5fbc3
Meghea Iulian <iulian.meghea@gmail.com>
2023-02-05 19:01:25 +0200

    feat(homes/firefox): init

---
+++ homes/firefox.nix
@@ -0,0 +1,63 @@
+{ pkgs, ... }:
+let
+  settings = {
+    "browser.startup.homepage" = "https://duckduckgo.com/";
+    "browser.search.defaultenginename" = "DuckDuckGo";
+    # Enable HTTPS-Only Mode
+    "dom.security.https_only_mode" = true;
+    "dom.security.https_only_mode_ever_enabled" = true;
+    # Privacy settings
+    "privacy.donottrackheader.enabled" = true;
+    "privacy.trackingprotection.enabled" = true;
+    "privacy.trackingprotection.socialtracking.enabled" = true;
+    "privacy.partition.network_state.ocsp_cache" = true;
+
+    "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+    "browser.newtabpage.activity-stream.telemetry" = false;
+    "browser.ping-centre.telemetry" = false;
+    "toolkit.telemetry.archive.enabled" = false;
+    "toolkit.telemetry.bhrPing.enabled" = false;
+    "toolkit.telemetry.enabled" = false;
+    "toolkit.telemetry.firstShutdownPing.enabled" = false;
+    "toolkit.telemetry.hybridContent.enabled" = false;
+    "toolkit.telemetry.newProfilePing.enabled" = false;
+    "toolkit.telemetry.reportingpolicy.firstRun" = false;
+    "toolkit.telemetry.shutdownPingSender.enabled" = false;
+    "toolkit.telemetry.unified" = false;
+    "toolkit.telemetry.updatePing.enabled" = false;
+    "experiments.activeExperiment" = false;
+    "experiments.enabled" = false;
+    "experiments.supported" = false;
+    "network.allow-experiments" = false;
+    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+    "extensions.pocket.enabled" = false;
+    "extensions.pocket.api" = "";
+    "extensions.pocket.oAuthConsumerKey" = "";
+    "extensions.pocket.showHome" = false;
+    "extensions.pocket.site" = "";
+  };
+in
+{
+  programs.firefox = {
+    enable = true;
+    extensions = [
+      pkgs.nur.repos.rycee.firefox-addons.ublock-origin
+      pkgs.nur.repos.rycee.firefox-addons.libredirect
+    ];
+    profiles.default = {
+      id = 0;
+      name = "Default";
+      inherit settings;
+    };
+    profiles.oather = {
+      id = 1;
+      name = "Oather";
+      inherit settings;
+    };
+  };
+
+  home.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "0";
+  };
+
+}
