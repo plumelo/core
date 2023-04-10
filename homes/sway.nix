d07ab2b0fa9e60403fecdf61b1bd37de0e47005b
Meghea Iulian <iulian.meghea@gmail.com>
2023-04-10 11:12:25 +0300

    fix(i3-status): adjust margins

--- homes/sway.nix
+++ homes/sway.nix
@@ -188,18 +188,18 @@ in
       }
       {
         block = "memory";
-        format = " $icon $mem_used.eng(prefix:M) ($mem_used_percents.eng(w:2)) ";
-        format_alt = " $icon_swap $swap_free.eng(w:3,u:B,p:M)/$swap_total.eng(w:3,u:B,p:M)($swap_used_percents.eng(w:2)) ";
+        format = "$icon $mem_used.eng(prefix:M) ($mem_used_percents.eng(w:2))";
+        format_alt = "$icon_swap $swap_free.eng(w:3,u:B,p:M)/$swap_total.eng(w:3,u:B,p:M)($swap_used_percents.eng(w:2))";
       }
       {
         block = "cpu";
         format = "$icon $utilization";
-        format_alt = " $icon $barchart $frequency ";
+        format_alt = "$icon $barchart $frequency";
       }
       {
         block = "temperature";
         interval = 10;
-        format = " $icon $max";
+        format = "$icon $max";
         chip = "k10temp-*";
         idle = 70;
         info = 75;
